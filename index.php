9df154fd1dcae7c3ec93b4c26e77d7852296a7d6
Redouan Atlati <ratlati@csumb.edu>
2018-02-14 16:44:05 +0000

    test

--- index.php
+++ index.php
@@ -5,10 +5,12 @@
   
   <h1>Redouan Atlati project</h1>
   <h2>Lab</h2>
-  <a href="portfolio"> Portfolio Lab 1 </a>
+  <a href="portfolio"> Portfolio Lab 1 </a><br></br>
+  <a href="Lab2"> Lab 2 </a>
   
   <h2>Lab</h2>
   <a href="homework1"> Homework 1 </a>
+  
   
 
   
